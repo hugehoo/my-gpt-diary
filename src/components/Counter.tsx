874abc94b0db1ca2d21f0c9958f3b3b0625d763c
hoo <tbnsok40@gmail.com>
2024-03-30 10:56:13 +0900

    feat: component 분리

---
+++ src/components/Counter.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+  const [userInput, setUserInput] = useState("")
+
+  const handleClickPlus = () => {
+    setCount(count + 1);
+  };
+
+  const handleClickMinus = () => {
+    setCount(count - 1);
+  }
+
+  const handleUserInput = (e: any) => {
+    setUserInput(e.target.value);
+  }
+
+  const handleEnter = (e: any) => {
+    if (e.key === "Enter") {
+      setUserInput("");
+      const num = Number(userInput);
+      if (Number.isInteger(num)) setCount(num);
+    }
+  };
+
+  return (
+    <>
+      <div>current count : {count}</div>
+      <div>
+        <div>count value input : </div>
+        <input value={userInput}
+               onChange={handleUserInput}
+               onKeyDown={handleEnter}
+        />
+      </div>
+      <div>
+        <div>buttons : </div>
+        <button onClick={handleClickPlus}>+</button>
+        <button onClick={handleClickMinus}>-</button>
+      </div>
+    </>
+  )
+}
+
+export default Counter;
